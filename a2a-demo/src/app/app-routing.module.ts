345640241916f175b0dca06cc193ffa49388a12f
Emiliano Suñé <emiliano.sune@gmail.com>
2020-01-20 12:48:49 -0800

    Initial app skeleton
    Signed-off-by: Emiliano Suñé <emiliano.sune@gmail.com>

---
+++ a2a-demo/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { KeycloakGuard } from './guards/keycloak-guard.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [KeycloakGuard]
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
